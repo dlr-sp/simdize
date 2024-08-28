59b476734ce99bfc7f8a2112e52acd665e18fb6d
Olaf Krzikalla <olaf.krzikalla@dlr.de>
2024-08-28 15:29:39 +0200

    Improved the computer benchmark

--- benchmark/compute_bm.cpp
+++ benchmark/compute_bm.cpp
@@ -11,16 +11,17 @@ namespace sa = simd_access;
 template<class T>
 using fixed_simd = stdx::fixed_size_simd<T, stdx::native_simd<T>::size()>;
 
+template<class T>
 void HeatCPUAndStack(benchmark::State& state)
 {
-  fixed_simd<double> data[1000];
+  T data[1000];
   for (int i = 0; i < 1000; ++i)
   {
     data[i] = 1.;
   }
-  benchmark::DoNotOptimize(data);
   for (auto _ : state)
   {
+    benchmark::DoNotOptimize(data);
     for (int i = 0; i < 1000; i += 4)
     {
       auto result = data[i] + data[i+1] * data[i+2] / data[i+3];
@@ -34,37 +35,34 @@ enum class Operation { Add, Sub, Mul, Div };
 template<class T, Operation OP>
 void SingleComputation(benchmark::State& state)
 {
-  T x, y;
-  benchmark::DoNotOptimize(x);
-  benchmark::DoNotOptimize(y);
+  T result, data[2] = { 1., 1. };
 
   for (auto _ : state)
   {
+    benchmark::DoNotOptimize(data);
     if constexpr (OP == Operation::Add)
     {
-      auto result = x + y;
-      benchmark::DoNotOptimize(result);
+      result = data[0] + data[1];
     }
     else if constexpr (OP == Operation::Sub)
     {
-      auto result = x - y;
-      benchmark::DoNotOptimize(result);
+      result = data[0] - data[1];
     }
     else if constexpr (OP == Operation::Mul)
     {
-      auto result = x * y;
-      benchmark::DoNotOptimize(result);
+      result = data[0] * data[1];
     }
     else if constexpr (OP == Operation::Div)
     {
-      auto result = x / y;
-      benchmark::DoNotOptimize(result);
+      result = data[0] / data[1];
     }
+    benchmark::DoNotOptimize(result);
   }
 }
 
 
-BENCHMARK(HeatCPUAndStack);
+BENCHMARK_TEMPLATE(HeatCPUAndStack, double);
+BENCHMARK_TEMPLATE(HeatCPUAndStack, fixed_simd<double>);
 BENCHMARK_TEMPLATE(SingleComputation, double, Operation::Add);
 BENCHMARK_TEMPLATE(SingleComputation, fixed_simd<double>, Operation::Add);
 BENCHMARK_TEMPLATE(SingleComputation, double, Operation::Mul);
